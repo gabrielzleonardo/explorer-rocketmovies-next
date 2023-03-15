8aef77421ebdfa25bc323ef9f216a28fe847d321
gabriel <gleonardoalano@gmail.com>
2023-03-15 17:44:28 -0300

    feat: button compenent

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -2,18 +2,25 @@ import { FiArrowLeft } from "react-icons/fi";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function BackButton({ content = "Voltar", href }) {
-  const router = useRouter();
+// export default function BackButton({ content = "Voltar", href }) {
+//   const router = useRouter();
 
-  return (
-    <button
-      href={href}
-      type="button"
-      className="secondaryTextButton text-center text-base flex "
-      onClick={() => (href ? router.push(href) : router.back(href))}
-    >
-      <FiArrowLeft className="text-2xl" />
-      <span className="ml-2">{content}</span>
-    </button>
-  );
+//   return (
+//     <button
+//       href={href}
+//       type="button"
+//       className="secondaryTextButton text-center text-base flex "
+//       onClick={() => (href ? router.push(href) : router.back(href))}
+//     >
+//       <FiArrowLeft className="text-2xl" />
+//       <span className="ml-2">{content}</span>
+//     </button>
+//   );
+// }
+
+export default function BackButton({ content = "Voltar", href, hasIcon = false, theme }) {
+  const themes  = {
+    textButton: 
+  }
+
 }
